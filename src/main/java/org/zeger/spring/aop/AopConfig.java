c565b6979970f833200fdcf8227e2838d65ec7b4
PavelZeger <gabberoid@gmail.com>
2021-03-28 22:10:10 +0300

    AOP - solution

--- src/main/java/org/zeger/spring/aop/AopConfig.java
+++ src/main/java/org/zeger/spring/aop/AopConfig.java
@@ -2,8 +2,6 @@ package org.zeger.spring.aop;
 
 import org.springframework.context.annotation.*;
 
-import java.util.function.Function;
-
 /**
  * @author Pavel Zeger
  * @implNote java-developer-course
